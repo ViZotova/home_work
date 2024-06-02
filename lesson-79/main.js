1aaad79c5406a4a22ec78289bdd87ff69262e051
Viktoria Zotova <150189673+ViZotova@users.noreply.github.com>
2024-06-02 17:41:58 +0500

    lesson-79

---
+++ lesson-79/main.js
@@ -0,0 +1,10 @@
+let age = 31
+
+// if (age >= 18) {
+//     console.log(`Вы совершеннолетний`)
+// } else {
+//     console.log(`Вам еще нет 18`)
+// }
+
+age >= 18 ? console.log(`Вы совершеннолетний`) : console.log(`Вам еще нет 18`)
+
