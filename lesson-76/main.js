a69eaf5ed0f69a9c00ea41eda68a3c4ffd2737b8
Viktoria Zotova <150189673+ViZotova@users.noreply.github.com>
2024-06-02 15:09:51 +0500

    lesson-76

---
+++ lesson-76/main.js
@@ -0,0 +1,7 @@
+const firstName ='Viktoriya'
+const lastName = 'Zotova'
+let age = 31
+const birthday = '23.07.1992'
+let toLive = 'Moscow'
+
+console.log(firstName, lastName, age, 'y.o', birthday, toLive)
