52efe213aabc6856a80e34f4d96aae3411b96bae
Viktoria Zotova <150189673+ViZotova@users.noreply.github.com>
2024-06-03 15:14:50 +0500

    Update main.js

--- lesson-81/main.js
+++ lesson-81/main.js
@@ -1,3 +1,16 @@
+// const numbers = [1, 2, 3, 4, 5]
+// const numbers = [1, '2', [1, 2, 3], 5]
+
+// numbers[4] = 6
+
+// console.log(numbers)
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i] + 1)
+// }
+
 // 1 Создайте произвольный массив чисел. Используя цикл for обойдите этот массив и выведите в консоль только четные числа.
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -21,3 +34,7 @@ rainbow [7] = 'purple'
 for (let i = 7; i >= 1; i = i - 1) {
     console.log(rainbow [i])
 }
+
+// for (let i = 0; i < rainbow.length; i = i - 1) {
+//     console.log(rainbow [1])
+// }
