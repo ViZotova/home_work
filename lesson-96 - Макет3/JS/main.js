86f10b8d1ff95011ae4a1fdabbca09fad33cc759
Viktoria Zotova <150189673+ViZotova@users.noreply.github.com>
2024-07-02 23:11:33 +0500

    Update main.js

--- lesson-96 - Макет3/JS/main.js
+++ lesson-96 - Макет3/JS/main.js
@@ -46,4 +46,27 @@
 
     }
 
+
+const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.about__img-button')
+
+    modalButton.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
+
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    }
+
+    function closeModal(e) {
+        e.preventDefault()
+
+        const target = e.target
+
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
+        }
+
+    }
+
 })()
