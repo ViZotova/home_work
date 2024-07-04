096b14c7d095abf9d13f9b24ff7ab2d8de824350
Viktoria Zotova <150189673+ViZotova@users.noreply.github.com>
2024-07-04 17:28:52 +0500

    Update main.js

--- lesson-96 - Макет3/JS/main.js
+++ lesson-96 - Макет3/JS/main.js
@@ -1,33 +1,3 @@
-// //https://fslightbox.com/javascript Для видео
-// //Бургер-меню - при нажатии на меню идет обработка события клика и показывать действия на клик / через модификатор body--opened-menu
-// //прописываем функцию и сразу вызываем, для менюшки адаптива / анонимная / конкретная область видимости без глобальной / чтобы не было конфликта одинаковых переменных / создаем локальную
-// (function() { 
-
-//    document.addEventListener('click', burgerInit) //делегирование событий / по клику будет воспроизводится функция
-//     function burgerInit(e) {
-
-//         const burgerIcon = e.target.closest('.burger-icon') // проверяет есть ли у элемента родитель с классом '' и если есть то функция возвращает родителя
-//         const burgerNavLink = e.target.closest('.nav__link') //элементы меню / возвращает родителя и если сам элемент nav__link
-
-//         // console.log(document.documentElement.clientWidth) Узнать размер ширины экрана клиенты
-
-
-//         if (!burgerIcon && !burgerNavLink) return //если нет БургерАйкон или БургернавЛинк / return - прекращаю выполнение
-
-//         if(document.documentElement.clientWidth > 900) return // есши ширина больше 900 то условия прерываются, если меньше то условия ниже срабатывают
-
-//         if (!document.body.classList.contains('body--opened-menu')) { //проверка
-//             document.body.classList.add('body--opened-menu')//добавляем еще класс если у body еще нет класса
-//             } else { //иначе его удаляем
-//             document.body.classList.remove('body--opened-menu')
-//         }
-
-//     }
-
-// })()
-
-
-
 (function() {
     //Бургер функция
     document.addEventListener('click', burgerInit)
@@ -66,40 +36,10 @@
         const target = e.target // переменная таргет = сам элемент на которы кликнули внутри модального окна
 
         if (target.closest('.modal__cancel') || target.classList.contains('modal')) { 
-            // 2 условия - должно работать одно из условий 1 - элемент на который кликнули внутри модального окна должен быть модал-кэнсал или иметь родителя модал-кэнсол 2 - если таргет на который клинкули имееет класс - модал
             document.body.classList.remove('body--opened-modal') //закрытие модального окна
         }
 
     }
-
-    //Табы
-
-    // const tabControls = document.querySelector('.tab-conrols') //переменная
-    // tabControls.addEventListener('click', toggleTab) //делегирование и отработка кликов / переключить вкладку на клик
-    // function toggleTab(e) { //объявление функции event = e
-    //     const tabControl = e.target.closest('.tab-conrols__link') // проверка на кнопку по переключению/ есть ли родитель   
-
-    //     if (!tabControl) return //если нет элемента то прекращаем функцию
-    //     e.preventDefault() //отмена перекидывания на др страницу
-    //     if (tabControl.classList.contains('tab-conrols__link--active')) return //если эта кнопка не активна / отменяем
-        
-
-    //     const tabContentID = tabControl.getAttribute('href') //переменная ID
-    //     const tabContent = document.querySelector(tabContentID) // переменная вкладки
-    //     const activeControl = document.querySelector('.tab-conrols__link--active')
-    //     const activeContent = document.querySelector('.tab-content--show')
-    //     //проверки
-
-    //     if (activeControl) {
-    //         activeControl.classList.remove('tab-conrols__link--active')//отмена
-    //     }
-    //     if(activeContent) {
-    //         activeContent.classList.remove('tab-content--show') //вкладки открытие перед этим нужно закрыть
-    //     }
-    //     //актуальность
-    //     tabControl.classList.add('tab-conrols__link--active')
-    //     tabContent.classList.add('tab-content--show')//навесить show на кнопку которую нажали / href
-    // }
 
     // Табы
 
