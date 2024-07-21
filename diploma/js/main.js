47c3ca8087aa8fdd12b2da907c6f79e41a237930
Viktoria Zotova <150189673+ViZotova@users.noreply.github.com>
2024-07-22 02:18:02 +0500

    Add files via upload

---
+++ diploma/js/main.js
@@ -0,0 +1,26 @@
+// //БУРГЕР
+function burgerMenu (selector) {
+    const menu = $(selector);
+    const button = menu.find('.burger-menu__button');
+    const links = menu.find('.burger-menu__link');
+    const overlay = menu.find('.burger-menu__overlay');
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu()); 
+    
+    function toggleMenu() {
+        menu.toggleClass('burger-menu-active');
+
+        if (menu.hasClass('burger-menu-active')) {
+            $('body').css('overflow', 'hidden');
+        } else {
+            $('body').css('overflow', 'visible');
+        }
+    }
+}
+burgerMenu('.header__block');
